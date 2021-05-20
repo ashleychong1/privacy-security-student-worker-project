c798e273711e90ab6ecb216152a35df1cc6db708
Your Name <you@example.com>
2021-05-19 21:20:08 -0400

    final

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,20 @@
 	"use strict";
 
 	// Add Scripts Here
-    	
+
+	function openMenu() {
+		var x = document.getElementById("mobile-home-container");
+		if (x.style.display === "none") {
+		  x.style.display = "flex";
+		} else {
+		  x.style.display = "none";
+		}
+		var x = document.getElementById("mobile-faq-container");
+		if (x.style.display === "none") {
+		  x.style.display = "flex";
+		} else {
+		  x.style.display = "none";
+		}
+	}
+
 })();
